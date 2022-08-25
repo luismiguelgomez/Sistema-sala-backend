283594d6265e85045de42401a6cde899d82ee9c3
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2022-08-24 23:59:37 -0500

    Creacion de proyecto Spring

---
+++ src/main/java/com/sala/edu/co/SistemaSala/SistemaSalaApplication.java
@@ -0,0 +1,13 @@
+package com.sala.edu.co.SistemaSala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sala/edu/co/SistemaSala/SistemaSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sala.edu.co.SistemaSala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
